59ef7f477a61f03d0b698fb7c4808dbb7b57408c
Jesalina <jesalinaphan@gmail.com>
2025-01-10 02:45:24 -0500

    final UI stylization - added logos

---
+++ frontend/src/components/BankBuddyLogo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BankBuddyLogo = () => (
+  <div className="flex items-center">
+    <img 
+      src="/bankbuddy-logo1.png" 
+      alt="" 
+      className="h-8 object-contain" 
+    />
+  </div>
+);
+
+export default BankBuddyLogo;
